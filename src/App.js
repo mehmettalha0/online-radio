32d5f0ee00e3a9ccb915be6d0a94e8143ceedbf2
talha <talhaaltinay07@gmail.com>
2023-02-04 23:41:16 +0300

    some init msg

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { DATA } from "./Data";
+import "./App.css";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "./img/superfm.png";
+
+function App() {
+  const [url, setUrl] = useState(null);
+  return (
+    <>
+      <div className="top">
+        <div className="header">Team Turkey - Online Radio</div>
+        <div className="App">
+          {DATA.map((item) => {
+            return (
+              <div className="null">
+                <div className="radio-image">
+                  <a
+                    onClick={() => {
+                      setUrl(item.url);
+                      var yazi = document.getElementById("yazi");
+                      yazi.innerHTML = `Şu anda Oynatılıyor: <b>${item.name}</b>`;
+                    }}
+                    className="radio-station"
+                  >
+                    <img src={item.image} />
+                  </a>
+                </div>
+                <div className="radio-name-list">
+                  <h1>{item.name}</h1>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <h1 id="yazi" className="radio-name">
+        <b>Şu anda bir şey oynamıyor</b>
+      </h1>
+      <div className="player">
+        <AudioPlayer
+          autoPlay
+          src={url}
+          showJumpControls={false}
+          showDownloadProgress={false}
+          showFilledProgress={false}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const DATA = [
+  {
+    id: 1,
+    name: "90'lar",
+    url: "http://37.247.98.8/stream/166/",
+    image: "https://pbs.twimg.com/media/C8emVdhXYAAfZKX.png",
+  },
+  {
+    id: 2,
+    name: "Best FM",
+    url: "http://46.20.7.125/",
+    image:
+      "https://upload.wikimedia.org/wikipedia/tr/archive/9/9b/20191115164648%21Best_FM.png",
+  },
+  {
+    id: 3,
+    name: "Fenomen Türkçe Rap",
+    url: "https://live.radyofenomen.com/fenomenturkrap/128/icecast.audio",
+    image: "https://cdn.radyofenomen.com/artwork/logo20.png",
+  },
+  {
+    id: 4,
+    name: "Dream Türk",
+    url: "https://radyo.duhnet.tv/dreamturk",
+    image:
+      "https://upload.wikimedia.org/wikipedia/tr/archive/b/bc/20200411150543%21Dream_T%C3%BCrk_logosu.png",
+  },
+  {
+    id: 5,
+    name: "Kral Pop",
+    url: "http://kralpopwmp.radyotvonline.com/",
+    image:
+      "https://upload.wikimedia.org/wikipedia/tr/thumb/9/9f/Kral_Pop_logosu.png/836px-Kral_Pop_logosu.png",
+  },
+  {
+    id: 6,
+    name: "Vırgın Radyo",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/VIRGIN_RADIO_SC?/",
+    image:
+      "https://upload.wikimedia.org/wikipedia/tr/0/07/Virgin_Radio_logo.png",
+  },
+];
